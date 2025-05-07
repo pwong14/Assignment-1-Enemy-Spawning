be7dcc7ea565a6d1afd7e3904dd12e5288967440
polarizz <polarizz39@gmail.com>
2025-05-06 18:37:15 -0700

    Initialized SpellBuilder

--- Assets/Scripts/Spells/SpellBuilder.cs
+++ Assets/Scripts/Spells/SpellBuilder.cs
@@ -5,8 +5,11 @@ using Newtonsoft.Json;
 using System.Collections.Generic;
 
 
-public class SpellBuilder 
+
+public class SpellBuilder
 {
+
+    Dictionary<string, Spell> spell_types = new Dictionary<string, Spell>();
 
     public Spell Build(SpellCaster owner)
     {
@@ -16,6 +19,20 @@ public class SpellBuilder
    
     public SpellBuilder()
     {        
+
+    }
+
+    void Start()
+    {
+        var spelltext = Resources.Load<TextAsset>("spells");
+
+        JToken jo = JToken.Parse(spelltext.text);
+
+        foreach (var spell in jo) {
+            Spell s = spell.ToObject<Spell>();
+            spell_types[s.GetName()] = s;
+            Debug.Log(s.GetName());
+        }
     }
 
 }
